76626b903060d6398c9b2b521e149dddcf68a279
Hlebsher <104029468+Hlebsher@users.noreply.github.com>
2022-05-21 19:14:44 +0300

    Update web_srgan.py

--- web_srgan.py
+++ web_srgan.py
@@ -1,3 +1,16 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from PIL import Image
+import cv2
+
+from skimage.metrics import peak_signal_noise_ratio as PSNR
+from skimage.metrics import structural_similarity as SSIM
+
+from utils import convert_image, center_crop, convert_to_y_channel
+from models import Generator
+
 import streamlit as st
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
