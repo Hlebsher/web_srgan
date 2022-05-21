6d9694e49d90d9d8574d36abb78928bead977625
Hlebsher <104029468+Hlebsher@users.noreply.github.com>
2022-05-21 19:19:13 +0300

    Update web_srgan.py

--- web_srgan.py
+++ web_srgan.py
@@ -5,8 +5,6 @@ import json
 from PIL import Image
 import cv2
 
-from skimage.metrics import peak_signal_noise_ratio as PSNR
-from skimage.metrics import structural_similarity as SSIM
 
 from utils import convert_image, center_crop, convert_to_y_channel
 from models import Generator
